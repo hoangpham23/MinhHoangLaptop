dbb5459abf079476082308db952e6473c8557325
aziky <pcm230304@gmail.com>
2023-11-24 13:10:35 +0700

    first commit

---
+++ Resource/js/mycode.js
@@ -0,0 +1,9 @@
+function openNav(){
+    var open = document.getElementById("mySidenav");
+    open.style.width = "300px";
+}
+
+function closeNav(){
+    var close = document.getElementById("mySidenav");
+    close.style.width = "0px";
+}
